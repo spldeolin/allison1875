a220c816b8b43f1815b682bf87f79c8e42c31169
Deolin <splendid.deolin@gmail.com>
2020-02-10 17:09:21 +0800

    Config加追加属性reportCollectorDetailOrNot（collector是否打印详细日志）

--- base/src/main/java/com/spldeolin/allison1875/base/Config.java
+++ base/src/main/java/com/spldeolin/allison1875/base/Config.java
@@ -3,6 +3,7 @@ package com.spldeolin.allison1875.base;
 import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import org.apache.commons.lang3.BooleanUtils;
 import org.yaml.snakeyaml.Yaml;
 import lombok.Data;
 
@@ -11,10 +12,24 @@ import lombok.Data;
  */
 public class Config {
 
+    /**
+     * collector是否打印详细日志
+     */
+    private Boolean reportCollectorDetailOrNot;
+
+    /**
+     * 项目源码路径
+     */
     private Path projectPath;
 
+    /**
+     * 项目打包后的war文件或是Spring Boot fat jar文件的路径
+     */
     private Path warOrFatJarPath;
 
+    /**
+     * 分页包装类的全限定名
+     */
     private String commonPageTypeQualifier;
 
     private static Config instance = new Config();
@@ -23,6 +38,7 @@ public class Config {
         Yaml yaml = new Yaml();
         InputStream is = this.getClass().getClassLoader().getResourceAsStream("config.yml");
         Map map = yaml.loadAs(is, Map.class);
+        reportCollectorDetailOrNot = BooleanUtils.toBoolean(map.reportCollectorDetailOrNot);
         projectPath = Paths.get(map.projectPath);
         warOrFatJarPath = Paths.get(map.warOrFatJarPath);
         commonPageTypeQualifier = map.commonPageTypeQualifier;
@@ -42,6 +58,8 @@ public class Config {
 
     @Data
     public static class Map {
+
+        String reportCollectorDetailOrNot;
 
         String projectPath;
 
