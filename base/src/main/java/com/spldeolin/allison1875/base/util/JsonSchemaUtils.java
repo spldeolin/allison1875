af9f891df50446e1006c2aeaa4759f202d849084
Deolin <splendid.deolin@gmail.com>
2020-04-20 12:12:00 +0800

    JsonSchemaUtils不再重复创建JavaType；generateSchema追加参数是Class<?>的重载方法

--- base/src/main/java/com/spldeolin/allison1875/base/util/JsonSchemaUtils.java
+++ base/src/main/java/com/spldeolin/allison1875/base/util/JsonSchemaUtils.java
@@ -1,6 +1,7 @@
 package com.spldeolin.allison1875.base.util;
 
 import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
@@ -15,26 +16,37 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class JsonSchemaUtils {
 
-    private static final int prefixLength = "urn:jsonschema:".length();
+    private static final String idPrefix = "urn:jsonschema:";
 
     private static final JsonSchemaGenerator defaultJsonSchemaGenerator = new JsonSchemaGenerator(
             JsonUtils.initObjectMapper(new ObjectMapper()));
+
+    private static final TypeFactory typeFactory = new TypeFactory(null) {
+
+        private static final long serialVersionUID = 2221941743132252200L;
+
+        @Override
+        public ClassLoader getClassLoader() {
+            return WarOrFatJarClassLoaderFactory.getClassLoader();
+        }
+    };
 
     private JsonSchemaUtils() {
         throw new UnsupportedOperationException("Never instantiate me.");
     }
 
+    public static JsonSchema generateSchema(Class<?> clazz) throws JsonSchemasException {
+        try {
+            return defaultJsonSchemaGenerator.generateSchema(clazz);
+        } catch (JsonMappingException e) {
+            log.warn("Cannot generate the json schema, clazz={}, reason={}", clazz, e.getMessage());
+            throw new JsonSchemasException();
+        }
+    }
+
     public static JsonSchema generateSchema(String qualifierForClassLoader) throws JsonSchemasException {
         try {
-            JavaType javaType = new TypeFactory(null) {
-
-                private static final long serialVersionUID = 2221941743132252200L;
-
-                @Override
-                public ClassLoader getClassLoader() {
-                    return WarOrFatJarClassLoaderFactory.getClassLoader();
-                }
-            }.constructFromCanonical(qualifierForClassLoader);
+            JavaType javaType = typeFactory.constructFromCanonical(qualifierForClassLoader);
             return defaultJsonSchemaGenerator.generateSchema(javaType);
         } catch (Exception e) {
             log.warn("Cannot generate the json schema, qualifierForClassLoader={}, reason={}", qualifierForClassLoader,
@@ -52,7 +64,7 @@ public class JsonSchemaUtils {
     }
 
     public static String getId(JsonSchema jsonSchema) {
-        return jsonSchema.getId().substring(prefixLength).replace(':', '.');
+        return jsonSchema.getId().substring(idPrefix.length()).replace(':', '.');
     }
 
 }
