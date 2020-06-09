99283671a871278901323d3011e34c29f611fcfa
Deolin <splendid.deolin@gmail.com>
2020-06-09 09:15:10 +0800

    删除多余的常量

--- base/src/main/java/com/spldeolin/allison1875/base/constant/QualifierConstants.java
+++ base/src/main/java/com/spldeolin/allison1875/base/constant/QualifierConstants.java
@@ -7,28 +7,6 @@ public interface QualifierConstants {
 
     String CONTROLLER = "org.springframework.stereotype.Controller";
 
-    String REST_CONTROLLER = "org.springframework.web.bind.annotation.RestController";
-
-    String DELETE_MAPPING = "org.springframework.web.bind.annotation.DeleteMapping";
-
-    String GET_MAPPING = "org.springframework.web.bind.annotation.GetMapping";
-
-    String PATCH_MAPPING = "org.springframework.web.bind.annotation.PatchMapping";
-
-    String POST_MAPPING = "org.springframework.web.bind.annotation.PostMapping";
-
-    String PUT_MAPPING = "org.springframework.web.bind.annotation.PutMapping";
-
-    String REQUEST_MAPPING = "org.springframework.web.bind.annotation.RequestMapping";
-
-    String COLLECTION = "java.util.Collection";
-
-    String MAP = "java.util.Map";
-
-    String MULTIMAP = "com.google.common.collect.Multimap";
-
-    String INTEGER = "java.lang.Integer";
-
     String LONG = "java.lang.Long";
 
     String REQUEST_BODY = "org.springframework.web.bind.annotation.RequestBody";
@@ -40,9 +18,5 @@ public interface QualifierConstants {
     String MULTIPART_FILE = "org.springframework.web.multipart.MultipartFile";
 
     String MYBATIS_PLUS_BASE_MAPPER = "com.baomidou.mybatisplus.mapper.BaseMapper";
-
-    String THROWABLE = "java.lang.Throwable";
-
-    String JSON_PROPERTY_DESCRIPTION = "com.fasterxml.jackson.annotation.JsonPropertyDescription";
 
 }
