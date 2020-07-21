f144cc5308ef00d424e80100fff5f471276ca653
Deolin <splendid.deolin@gmail.com>
2020-07-21 08:04:15 +0800

    persistence-generator生成Entity时能够反映变动点

--- persistence-generator/src/main/java/com/spldeolin/allison1875/persistencegenerator/processor/EntityProcessor.java
+++ persistence-generator/src/main/java/com/spldeolin/allison1875/persistencegenerator/processor/EntityProcessor.java
@@ -5,28 +5,35 @@ import static com.github.javaparser.StaticJavaParser.parseAnnotation;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.util.TreeSet;
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Modifier.Keyword;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.comments.JavadocComment;
 import com.github.javaparser.javadoc.Javadoc;
 import com.github.javaparser.javadoc.JavadocBlockTag;
 import com.github.javaparser.javadoc.JavadocBlockTag.Type;
 import com.github.javaparser.utils.CodeGenerationUtils;
+import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.google.common.collect.Sets.SetView;
 import com.spldeolin.allison1875.base.creator.CuCreator;
 import com.spldeolin.allison1875.persistencegenerator.PersistenceGeneratorConfig;
 import com.spldeolin.allison1875.persistencegenerator.constant.Constant;
 import com.spldeolin.allison1875.persistencegenerator.javabean.PersistenceDto;
 import com.spldeolin.allison1875.persistencegenerator.javabean.PropertyDto;
 import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
+import lombok.extern.log4j.Log4j2;
 
 /**
  * @author Deolin 2020-07-18
  */
-@Slf4j
+@Log4j2
 public class EntityProcessor {
 
     private final PersistenceDto persistence;
@@ -45,11 +52,23 @@ public class EntityProcessor {
         PersistenceGeneratorConfig conf = PersistenceGeneratorConfig.getInstace();
         entityPath = CodeGenerationUtils.fileInPackageAbsolutePath(conf.getSourceRoot(), conf.getEntityPackage(),
                 persistence.getEntityName() + ".java");
+
+        TreeSet<String> originalVariables = Sets.newTreeSet();
         if (entityPath.toFile().exists()) {
+            try {
+                CompilationUnit cu = StaticJavaParser.parse(entityPath);
+                for (FieldDeclaration field : cu.findAll(FieldDeclaration.class)) {
+                    for (VariableDeclarator variable : field.getVariables()) {
+                        originalVariables.add(variable.getTypeAsString() + " " + variable.getNameAsString());
+                    }
+                }
+            } catch (Exception e) {
+                log.warn("StaticJavaParser.parse failed entityPath={}", entityPath, e);
+            }
             log.info("Entity file exist, overwrite. [{}]", entityPath);
         }
 
-        // 覆盖生成Entity
+        // 生成Entity（可能是覆盖）
         entityCuCreator = new CuCreator(Paths.get(conf.getSourceRoot()), conf.getEntityPackage(),
                 Lists.newArrayList("java.math.BigDecimal", "java.util.Date", "lombok.Data",
                         "lombok.experimental.Accessors"), () -> {
@@ -64,14 +83,36 @@ public class EntityProcessor {
             coid.setPublic(true);
             coid.setName(persistence.getEntityName());
             for (PropertyDto property : persistence.getProperties()) {
-                FieldDeclaration field = coid
-                        .addField(property.getJavaType(), property.getPropertyName(), Keyword.PRIVATE);
+                String type = property.getJavaType().getSimpleName();
+                String name = property.getPropertyName();
+                FieldDeclaration field = coid.addField(type, name, Keyword.PRIVATE);
                 Javadoc fieldJavadoc = new JavadocComment(property.getDescription()).parse();
                 fieldJavadoc.addBlockTag(new JavadocBlockTag(Type.SEE, property.getColumnName()));
                 field.setJavadocComment(fieldJavadoc);
+
             }
             return coid;
         });
+
+        TreeSet<String> destinedVariables = Sets.newTreeSet();
+        for (PropertyDto property : persistence.getProperties()) {
+            destinedVariables.add(property.getJavaType().getSimpleName() + " " + property.getPropertyName());
+        }
+
+        if (!originalVariables.isEmpty()) {
+            SetView<String> delete = Sets.difference(originalVariables, destinedVariables);
+            SetView<String> add = Sets.difference(destinedVariables, originalVariables);
+            if (add.size() > 0) {
+                log.info("{} 将会新增属性 {}", persistence.getEntityName(), Joiner.on(", ").join(add));
+            }
+            if (delete.size() > 0) {
+                log.info("{} 中的属性 {} 将会被删除", persistence.getEntityName(), Joiner.on(", ").join(delete));
+            }
+            if (add.size() == 0 && delete.size() == 0) {
+                log.info("{} 中没有属性增减", persistence.getEntityName());
+            }
+
+        }
 
         return this;
     }
