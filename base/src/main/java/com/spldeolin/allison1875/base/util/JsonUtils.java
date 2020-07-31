5c698a6d498f372fea0f8aabf9c970f29040422f
Deolin <splendid.deolin@gmail.com>
2020-07-31 15:40:24 +0800

    JsonUtils

--- base/src/main/java/com/spldeolin/allison1875/base/util/JsonUtils.java
+++ base/src/main/java/com/spldeolin/allison1875/base/util/JsonUtils.java
@@ -1,24 +1,36 @@
 package com.spldeolin.allison1875.base.util;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.TimeZone;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import com.spldeolin.allison1875.base.util.exception.JsonException;
 import lombok.extern.log4j.Log4j2;
 
 /**
  * JSON工具类
+ *
  * <pre>
- * 支持JSON 与对象间、与对象列表间 的互相转换。
+ * 支持JSON与对象间的互相转换
  * </pre>
  *
- * @author Deolin
+ * @author Deolin 2018-04-02
  */
 @Log4j2
 public class JsonUtils {
@@ -30,8 +42,14 @@ public class JsonUtils {
     }
 
     public static ObjectMapper initObjectMapper(ObjectMapper om) {
-        // json -> object时，忽略json中不认识的属性名
+        // Java8时间类型的全局格式
+        om.registerModule(timeModule());
+
+        // 反序列化时，忽略json中存在，但Javabean中不存在的属性
         om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        // 时区
+        om.setTimeZone(TimeZone.getDefault());
 
         // 只有类属性可见，类的getter、setter、构造方法里的字段不会被当作JSON的字段
         om.setVisibility(om.getSerializationConfig().getDefaultVisibilityChecker()
@@ -39,29 +57,21 @@ public class JsonUtils {
                 .withSetterVisibility(JsonAutoDetect.Visibility.NONE)
                 .withCreatorVisibility(JsonAutoDetect.Visibility.NONE));
 
-        // 时区
-        om.setTimeZone(TimeZone.getDefault());
         return om;
     }
 
-    /**
-     * 美化JSON
-     * <pre>
-     * e.g.:
-     * {
-     *   "name" : "Deolin",
-     *   "age" : 18,
-     *   "isVip" : true
-     * }
-     * </pre>
-     */
-    public static String beautify(Object object) {
-        try {
-            return om.writerWithDefaultPrettyPrinter().writeValueAsString(object);
-        } catch (JsonProcessingException e) {
-            log.error("object={}", object, e);
-            throw new JsonException("转化JSON失败");
-        }
+    public static SimpleModule timeModule() {
+        SimpleModule javaTimeModule = new JavaTimeModule();
+        DateTimeFormatter date = TimeUtils.DEFAULT_DATE_FORMATTER;
+        DateTimeFormatter time = TimeUtils.DEFAULT_TIME_FORMATTER;
+        DateTimeFormatter dateTime = TimeUtils.DEFAULT_DATE_TIME_FORMATTER;
+        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(date))
+                .addDeserializer(LocalDate.class, new LocalDateDeserializer(date))
+                .addSerializer(LocalTime.class, new LocalTimeSerializer(time))
+                .addDeserializer(LocalTime.class, new LocalTimeDeserializer(time))
+                .addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(dateTime))
+                .addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(dateTime));
+        return javaTimeModule;
     }
 
     /**
@@ -72,11 +82,30 @@ public class JsonUtils {
     }
 
     /**
-     * 将对象转化为JSON，支持自定义ObjectMapper
+     * 将对象转化为JSON
      */
     public static String toJson(Object object, ObjectMapper om) {
         try {
             return om.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            log.error("object={}", object, e);
+            throw new JsonException(e);
+        }
+    }
+
+    /**
+     * 将对象转化为JSON，结果是美化的
+     */
+    public static String toJsonPrettily(Object object) {
+        return toJsonPrettily(object, om);
+    }
+
+    /**
+     * 将对象转化为JSON，结果是美化的
+     */
+    public static String toJsonPrettily(Object object, ObjectMapper om) {
+        try {
+            return om.writerWithDefaultPrettyPrinter().writeValueAsString(object);
         } catch (JsonProcessingException e) {
             log.error("object={}", object, e);
             throw new JsonException("转化JSON失败");
@@ -85,42 +114,65 @@ public class JsonUtils {
 
     /**
      * 将JSON转化为对象
+     *
+     * @throws JsonException 转化失败时，抛出这个Runtime异常，如果需要补偿处理，可以捕获这个异常
      */
     public static <T> T toObject(String json, Class<T> clazz) {
         return toObject(json, clazz, om);
     }
 
     /**
-     * 将JSON转化为对象，支持自定义ObjectMapper
+     * 将JSON转化为对象
+     *
+     * @throws JsonException 转化失败时，抛出这个Runtime异常，如果需要补偿处理，可以捕获这个异常
      */
     public static <T> T toObject(String json, Class<T> clazz, ObjectMapper om) {
         try {
             return om.readValue(json, clazz);
         } catch (IOException e) {
             log.error("json={}, clazz={}", json, clazz, e);
-            throw new JsonException("转化对象失败");
+            throw new JsonException(e);
         }
     }
 
     /**
      * 将JSON转化为对象列表
+     *
+     * @throws JsonException 转化失败时，抛出这个Runtime异常，如果需要补偿处理，可以捕获这个异常
      */
-    public static <T> List<T> toListOfObjects(String json, Class<T> clazz) {
-        return toListOfObjects(json, clazz, om);
+    public static <T> List<T> toListOfObject(String json, Class<T> clazz) {
+        return toListOfObject(json, clazz, om);
     }
 
     /**
-     * 将JSON转化为对象列表，支持自定义ObjectMapper
+     * 将JSON转化为对象列表
+     *
+     * @throws JsonException 转化失败时，抛出这个Runtime异常，如果需要补偿处理，可以捕获这个异常
      */
-    public static <T> List<T> toListOfObjects(String json, Class<T> clazz, ObjectMapper om) {
-        // ObjectMapper.TypeFactory没有线程隔离，所以需要new一个默认ObjectMapper
-        TypeFactory typeFactory = initObjectMapper(new ObjectMapper()).getTypeFactory();
-        JavaType javaType = typeFactory.constructParametricType(List.class, clazz);
+    public static <T> List<T> toListOfObject(String json, Class<T> clazz, ObjectMapper om) {
         try {
-            return om.readValue(json, javaType);
+            return om.readValue(json, new TypeReference<List<T>>() {
+            });
         } catch (IOException e) {
             log.error("json={}, clazz={}", json, clazz, e);
-            throw new JsonException("转化对象失败");
+            throw new JsonException(e);
+        }
+    }
+
+    /**
+     * JSON -> 参数化的对象
+     *
+     * 示例： Collection<<User<UserAddress>> users = JsonUtils.toParameterizedObject(text);
+     *
+     * @throws JsonException 转化失败时，抛出这个Runtime异常，如果需要补偿处理，可以捕获这个异常
+     */
+    public static <T> T toParameterizedObject(String json) {
+        try {
+            return om.readValue(json, new TypeReference<T>() {
+            });
+        } catch (IOException e) {
+            log.error("json={}", json, e);
+            throw new JsonException(e);
         }
     }
 
