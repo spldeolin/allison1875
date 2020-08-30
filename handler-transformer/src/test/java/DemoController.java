d1920186f303f80ebd863b7b8226be6b74b6602c
Deolin <splendid.deolin@gmail.com>
2020-08-30 15:05:23 +0800

    handler-transformer追加示例

---
+++ handler-transformer/src/test/java/DemoController.java
@@ -0,0 +1,34 @@
+/**
+ * @author Deolin 2020-08-30
+ */
+public class DemoController {
+
+    {
+        String handler = "aademo", desc = "的事";
+        {
+            // // 用户ID
+            // @NotNull
+            // Long id;
+            // {
+            // String dto = "type";
+            // Collection<Integer> types;
+            // }
+            // {
+            // String dto = "date";
+            // Date date;
+            // }
+        }
+        {
+            // String aa;
+            // {
+            // // String dtos = "child";
+            // String childName;
+            // {
+            // String dtos$ = "grandChild";
+            // String grandChildName;
+            // }
+            // }
+        }
+    }
+
+}
