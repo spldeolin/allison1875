018fdd1f4b215f3dedb7f70f0c6709e97ef289cf
Deolin <splendid.deolin@gmail.com>
2020-10-06 15:33:21 +0800

    query-transformer：引入QueryMeta

---
+++ query-transformer/src/main/java/com/spldeolin/allison1875/querytransformer/javabean/QueryMeta.java
@@ -0,0 +1,58 @@
+package com.spldeolin.allison1875.querytransformer.javabean;
+
+/**
+ * @author Deolin 2020-10-06
+ */
+public class QueryMeta {
+
+    private String entityQualifier;
+
+    private String entityName;
+
+    private String mapperQualifier;
+
+    private String mapperName;
+
+    private String mapperRelativePath;
+
+    public String getEntityQualifier() {
+        return entityQualifier;
+    }
+
+    public void setEntityQualifier(String entityQualifier) {
+        this.entityQualifier = entityQualifier;
+    }
+
+    public String getEntityName() {
+        return entityName;
+    }
+
+    public void setEntityName(String entityName) {
+        this.entityName = entityName;
+    }
+
+    public String getMapperQualifier() {
+        return mapperQualifier;
+    }
+
+    public void setMapperQualifier(String mapperQualifier) {
+        this.mapperQualifier = mapperQualifier;
+    }
+
+    public String getMapperName() {
+        return mapperName;
+    }
+
+    public void setMapperName(String mapperName) {
+        this.mapperName = mapperName;
+    }
+
+    public String getMapperRelativePath() {
+        return mapperRelativePath;
+    }
+
+    public void setMapperRelativePath(String mapperRelativePath) {
+        this.mapperRelativePath = mapperRelativePath;
+    }
+
+}
