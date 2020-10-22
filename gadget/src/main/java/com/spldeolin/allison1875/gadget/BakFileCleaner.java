73daaafd4beb090c0075dcc61c6143982b7ff063
Deolin <splendid.deolin@gmail.com>
2020-10-22 14:10:35 +0800

    追加gadget模块；追加BakFileCleaner工具

---
+++ gadget/src/main/java/com/spldeolin/allison1875/gadget/BakFileCleaner.java
@@ -0,0 +1,25 @@
+package com.spldeolin.allison1875.gadget;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.Logger;
+import com.spldeolin.allison1875.base.ancestor.Allison1875MainProcessor;
+import com.spldeolin.allison1875.base.ast.AstForest;
+
+/**
+ * 删除宿主中所有bak文件的清理器
+ *
+ * @author Deolin 2020-10-22
+ */
+public class BakFileCleaner implements Allison1875MainProcessor {
+
+    private static final Logger log = org.apache.logging.log4j.LogManager.getLogger(BakFileCleaner.class);
+
+    @Override
+    public void process(AstForest astForest) {
+        FileUtils.iterateFiles(astForest.getHost().toFile(), new String[]{"bak"}, true).forEachRemaining(bakFile -> {
+            log.info(bakFile.toString() + " 被删除");
+            bakFile.deleteOnExit();
+        });
+    }
+
+}
