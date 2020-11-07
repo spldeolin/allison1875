ab53227d4f6002468f16e2ec3a36971de950b87d
Deolin <splendid.deolin@gmail.com>
2020-11-08 07:40:01 +0800

    persistence-generator：InsertXmlProc缩进优化

--- persistence-generator/src/main/java/com/spldeolin/allison1875/persistencegenerator/processor/mapperxml/InsertXmlProc.java
+++ persistence-generator/src/main/java/com/spldeolin/allison1875/persistencegenerator/processor/mapperxml/InsertXmlProc.java
@@ -36,6 +36,7 @@ public class InsertXmlProc extends XmlProc {
         }
         List<String> xmlLines = Lists.newArrayList();
         xmlLines.add(String.format("<insert id=\"%s\" parameterType=\"%s\">", insertProc.getMethodName(), entityName));
+        xmlLines.add(BaseConstant.SINGLE_INDENT + "<!-- @formatter:off -->");
         xmlLines.add(BaseConstant.SINGLE_INDENT + "INSERT INTO " + persistence.getTableName());
         xmlLines.add(BaseConstant.SINGLE_INDENT + "<trim prefix=\"(\" suffix=\")\" suffixOverrides=\",\">");
         for (PropertyDto property : persistence.getProperties()) {
@@ -50,33 +51,8 @@ public class InsertXmlProc extends XmlProc {
                             property.getPropertyName()));
         }
         xmlLines.add(BaseConstant.SINGLE_INDENT + "</trim>");
+        xmlLines.add(BaseConstant.SINGLE_INDENT + "<!-- @formatter:on -->");
         xmlLines.add("</insert>");
-//
-//        Element insertTag = new DefaultElement("insert");
-//        insertTag.addAttribute("id", insertProc.getMethodName());
-//        insertTag.addAttribute("parameterType", entityName);
-//        if (persistence.getPkProperties().size() > 0) {
-//            insertTag.addAttribute("useGeneratedKeys", "true");
-//            String keyProperty = persistence.getPkProperties().stream().map(PropertyDto::getColumnName)
-//                    .collect(Collectors.joining(", "));
-//            insertTag.addAttribute("keyProperty", keyProperty);
-//        }
-//        newLineWithIndent(insertTag);
-//        insertTag.addText("INSERT INTO ").addText(persistence.getTableName());
-//        Element trimTag1 = insertTag.addElement("trim").addAttribute("prefix", "(").addAttribute("suffix", ")")
-//                .addAttribute("suffixOverrides", ",");
-//        for (PropertyDto property : persistence.getProperties()) {
-//            Element ifTag = trimTag1.addElement("if");
-//            ifTag.addAttribute("test", property.getPropertyName() + "!=null");
-//            ifTag.addText(property.getColumnName() + ",");
-//        }
-//        Element trimTag2 = insertTag.addElement("trim").addAttribute("prefix", "VALUES (").addAttribute("suffix", ")")
-//                .addAttribute("suffixOverrides", ",");
-//        for (PropertyDto property : persistence.getProperties()) {
-//            Element ifTag = trimTag2.addElement("if");
-//            ifTag.addAttribute("test", property.getPropertyName() + "!=null");
-//            ifTag.addText("#{" + property.getPropertyName() + "},");
-//        }
 
         sourceCodeLines = xmlLines;
         return this;
