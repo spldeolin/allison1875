1b5948052abeaca803f562dfa3b9acf5acf57753
Deolin <splendid.deolin@gmail.com>
2020-11-08 07:40:54 +0800

    persistence-generator：BatchInsertEvenNullXmlProc缩进优化

--- persistence-generator/src/main/java/com/spldeolin/allison1875/persistencegenerator/processor/mapperxml/BatchInsertEvenNullXmlProc.java
+++ persistence-generator/src/main/java/com/spldeolin/allison1875/persistencegenerator/processor/mapperxml/BatchInsertEvenNullXmlProc.java
@@ -1,16 +1,12 @@
 package com.spldeolin.allison1875.persistencegenerator.processor.mapperxml;
 
-import static com.spldeolin.allison1875.base.constant.BaseConstant.SINGLE_INDENT;
-
 import java.util.Collection;
 import java.util.List;
-import com.google.common.collect.Iterables;
+import java.util.stream.Collectors;
 import com.google.common.collect.Lists;
 import com.spldeolin.allison1875.base.constant.BaseConstant;
-import com.spldeolin.allison1875.base.util.StringUtils;
 import com.spldeolin.allison1875.persistencegenerator.PersistenceGeneratorConfig;
 import com.spldeolin.allison1875.persistencegenerator.javabean.PersistenceDto;
-import com.spldeolin.allison1875.persistencegenerator.javabean.PropertyDto;
 import com.spldeolin.allison1875.persistencegenerator.processor.mapper.BatchInsertEvenNullProc;
 
 /**
@@ -37,21 +33,16 @@ public class BatchInsertEvenNullXmlProc extends XmlProc {
         }
         List<String> xmlLines = Lists.newArrayList();
         xmlLines.add(String.format("<insert id=\"%s\">", batchInsertEvenNullProc.getMethodName()));
-        xmlLines.add(SINGLE_INDENT + "<!-- @formatter:off -->");
+        xmlLines.add(BaseConstant.SINGLE_INDENT + "<!-- @formatter:off -->");
         xmlLines.add(BaseConstant.SINGLE_INDENT + String.format("INSERT INTO %s", persistence.getTableName()));
-        xmlLines.add(BaseConstant.SINGLE_INDENT + "(<include refid=\"all\"/>)");
+        xmlLines.add(BaseConstant.DOUBLE_INDENT + "( <include refid=\"all\"/> )");
         xmlLines.add(BaseConstant.SINGLE_INDENT + "VALUES");
         xmlLines
-                .add(BaseConstant.SINGLE_INDENT + "<foreach collection=\"entities\" item=\"one\" separator=\",\">");
-        xmlLines.add(BaseConstant.DOUBLE_INDENT + "(");
-        for (PropertyDto property : persistence.getProperties()) {
-            xmlLines.add(BaseConstant.DOUBLE_INDENT + "#{one." + property.getPropertyName() + "},");
-        }
-        xmlLines
-                .set(xmlLines.size() - 1, StringUtils.removeLast(Iterables.getLast(xmlLines), ","));
-        xmlLines.add(BaseConstant.DOUBLE_INDENT + ")");
-        xmlLines.add(BaseConstant.SINGLE_INDENT + "</foreach>");
-        xmlLines.add(SINGLE_INDENT + "<!-- @formatter:on -->");
+                .add(BaseConstant.DOUBLE_INDENT + "<foreach collection=\"entities\" item=\"one\" separator=\",\">");
+        xmlLines.add(BaseConstant.TREBLE_INDENT + "<!-- @formatter:on -->");
+        xmlLines.add(BaseConstant.TREBLE_INDENT + "( " + persistence.getProperties().stream()
+                .map(p -> "#{one." + p.getPropertyName() + "}").collect(Collectors.joining(", ")) + " )");
+        xmlLines.add(BaseConstant.DOUBLE_INDENT + "</foreach>");
         xmlLines.add("</insert>");
         this.sourceCodeLines = xmlLines;
         return this;
