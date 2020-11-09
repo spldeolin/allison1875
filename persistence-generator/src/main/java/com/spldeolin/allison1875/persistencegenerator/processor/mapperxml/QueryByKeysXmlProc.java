cd121cb2478d52f38e05b862fa7c94db2887a34e
Deolin <splendid.deolin@gmail.com>
2020-11-09 17:29:41 +0800

    persistence-generator：QueryByKeysXmlProc不再依赖dom4j，并追加@formatter:off防格式化措施

--- persistence-generator/src/main/java/com/spldeolin/allison1875/persistencegenerator/processor/mapperxml/QueryByKeysXmlProc.java
+++ persistence-generator/src/main/java/com/spldeolin/allison1875/persistencegenerator/processor/mapperxml/QueryByKeysXmlProc.java
@@ -1,13 +1,13 @@
 package com.spldeolin.allison1875.persistencegenerator.processor.mapperxml;
 
 import java.util.Collection;
-import org.dom4j.Element;
-import org.dom4j.tree.DefaultElement;
+import java.util.List;
 import com.google.common.collect.Lists;
+import com.spldeolin.allison1875.base.constant.BaseConstant;
 import com.spldeolin.allison1875.persistencegenerator.PersistenceGeneratorConfig;
+import com.spldeolin.allison1875.persistencegenerator.constant.Constant;
 import com.spldeolin.allison1875.persistencegenerator.javabean.PersistenceDto;
 import com.spldeolin.allison1875.persistencegenerator.processor.mapper.QueryByKeysProc;
-import com.spldeolin.allison1875.persistencegenerator.util.Dom4jUtils;
 
 /**
  * 根据外键列表查询，表中每有几个外键，这个Proc就生成几个方法
@@ -32,28 +32,25 @@ public class QueryByKeysXmlProc extends XmlProc {
             return this;
         }
         for (QueryByKeysProc queryByKeyProc : queryByKeysProcs) {
-            Element stmt = new DefaultElement("select");
-            stmt.addAttribute("id", queryByKeyProc.getMethodName());
-            addParameterType(stmt, queryByKeyProc.getKey());
-            stmt.addAttribute("resultMap", "all");
-            newLineWithIndent(stmt);
-            stmt.addText("SELECT");
-            stmt.addElement("include").addAttribute("refid", "all");
-            newLineWithIndent(stmt);
-            stmt.addText("FROM ").addText(persistence.getTableName());
-            newLineWithIndent(stmt);
-            stmt.addText("WHERE");
-            newLineWithIndent(stmt);
+            List<String> xmlLines = Lists.newArrayList();
+            xmlLines.add(String.format("<select id=\"%s\" parameterType=\"%s\" resultMap=\"all\">",
+                    queryByKeyProc.getMethodName(),
+                    queryByKeyProc.getKey().getJavaType().getName().replaceFirst("java\\.lang\\.", "")));
+            xmlLines.add(BaseConstant.SINGLE_INDENT + Constant.FORMATTER_OFF_MARKER);
+            xmlLines.add(BaseConstant.SINGLE_INDENT + "SELECT");
+            xmlLines.add(BaseConstant.DOUBLE_INDENT + "<include refid=\"all\"/>");
+            xmlLines.add(BaseConstant.SINGLE_INDENT + "FROM " + persistence.getTableName());
+            xmlLines.add(BaseConstant.SINGLE_INDENT + "WHERE TRUE");
             if (persistence.getIsDeleteFlagExist()) {
-                stmt.addText(PersistenceGeneratorConfig.getInstance().getNotDeletedSql());
-                newLineWithIndent(stmt);
-                stmt.addText("AND ");
+                xmlLines.add(BaseConstant.SINGLE_INDENT + "  AND " + PersistenceGeneratorConfig.getInstance()
+                        .getNotDeletedSql());
             }
-            stmt.addText(queryByKeyProc.getKey().getColumnName()).addText(" IN (");
-            stmt.addElement("foreach").addAttribute("collection", queryByKeyProc.getVarsName())
-                    .addAttribute("item", "one").addAttribute("separator", ",").addText("#{one}");
-            stmt.addText(")");
-            sourceCodeLines.addAll(Dom4jUtils.toSourceCodeLines(stmt));
+            xmlLines.add(BaseConstant.SINGLE_INDENT + "  AND " + queryByKeyProc.getKey().getColumnName() + String
+                    .format(" IN (<foreach collection=\"%s\" item=\"one\" separator=\",\">#{one}</foreach>)",
+                            queryByKeyProc.getVarsName()));
+            xmlLines.add(BaseConstant.SINGLE_INDENT + Constant.FORMATTER_ON_MARKER);
+            xmlLines.add("</select>");
+            sourceCodeLines.addAll(xmlLines);
         }
         return this;
     }
