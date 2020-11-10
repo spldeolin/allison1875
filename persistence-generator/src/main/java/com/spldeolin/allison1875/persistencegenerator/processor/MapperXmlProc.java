1ccbb25c756dfcbec0efd0fc87a6648d7ac0014e
Deolin <splendid.deolin@gmail.com>
2020-11-10 13:14:26 +0800

    移除dom4j

--- persistence-generator/src/main/java/com/spldeolin/allison1875/persistencegenerator/processor/MapperXmlProc.java
+++ persistence-generator/src/main/java/com/spldeolin/allison1875/persistencegenerator/processor/MapperXmlProc.java
@@ -4,13 +4,11 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.dom4j.Document;
-import org.dom4j.DocumentHelper;
-import org.dom4j.Element;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.google.common.collect.Lists;
 import com.spldeolin.allison1875.base.constant.BaseConstant;
@@ -20,7 +18,6 @@ import com.spldeolin.allison1875.base.util.StringUtils;
 import com.spldeolin.allison1875.persistencegenerator.constant.Constant;
 import com.spldeolin.allison1875.persistencegenerator.javabean.PersistenceDto;
 import com.spldeolin.allison1875.persistencegenerator.processor.mapperxml.XmlProc;
-import com.spldeolin.allison1875.persistencegenerator.util.Dom4jUtils;
 
 /**
  * @author Deolin 2020-07-18
@@ -51,13 +48,14 @@ public class MapperXmlProc {
         mapperXmlFile = mapperXmlDirectory.resolve(persistence.getMapperName() + ".xml").toFile();
 
         if (!mapperXmlFile.exists()) {
-            // create
-            Document document = DocumentHelper.createDocument();
-            document.addDocType("mapper",
-                    "-//mybatis.org//DTD Mapper 3.0//EN\" \"http://mybatis.org/dtd/mybatis-3-mapper.dtd", null);
-            Element root = document.addElement("mapper").addText(BaseConstant.NEW_LINE);
-            root.addAttribute("namespace", mapper.getFullyQualifiedName().orElseThrow(QualifierAbsentException::new));
-            Dom4jUtils.write(mapperXmlFile, document);
+            // create new File
+            Collection<String> sourceCodeLines = Lists.newArrayList();
+            sourceCodeLines.add("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+            sourceCodeLines.add("<!DOCTYPE mapper PUBLIC \"-//mybatis.org//DTD Mapper 3.0//EN\" \"http://mybatis"
+                    + ".org/dtd/mybatis-3-mapper.dtd\">");
+            sourceCodeLines.add(String.format("<mapper namespace=\"%s\">",
+                    mapper.getFullyQualifiedName().orElseThrow(QualifierAbsentException::new)));
+            FileUtils.writeLines(mapperXmlFile, sourceCodeLines);
         }
 
         List<String> newLines = Lists.newArrayList();
--- persistence-generator/src/main/java/com/spldeolin/allison1875/persistencegenerator/processor/mapperxml/XmlProc.java
+++ persistence-generator/src/main/java/com/spldeolin/allison1875/persistencegenerator/processor/mapperxml/XmlProc.java
@@ -1,8 +1,6 @@
 package com.spldeolin.allison1875.persistencegenerator.processor.mapperxml;
 
 import java.util.Collection;
-import org.dom4j.Element;
-import com.spldeolin.allison1875.base.constant.BaseConstant;
 
 /**
  * @author Deolin 2020-08-08
@@ -10,9 +8,5 @@ import com.spldeolin.allison1875.base.constant.BaseConstant;
 public abstract class XmlProc {
 
     public abstract Collection<String> getSourceCodeLines();
-
-    protected void newLineWithIndent(Element stmt) {
-        stmt.addText(BaseConstant.NEW_LINE).addText(BaseConstant.SINGLE_INDENT);
-    }
 
 }
