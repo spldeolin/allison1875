7c4e5a1e044d2f595607a369e4acecdee999cdae
Deolin <splendid.deolin@gmail.com>
2020-12-09 16:43:58 +0800

    fix bug

--- inspector/src/main/java/com/spldeolin/allison1875/inspector/InspectorModule.java
+++ inspector/src/main/java/com/spldeolin/allison1875/inspector/InspectorModule.java
@@ -1,9 +1,13 @@
 package com.spldeolin.allison1875.inspector;
 
+import java.util.Collection;
+import com.google.common.collect.Lists;
 import com.google.inject.Injector;
+import com.google.inject.TypeLiteral;
 import com.spldeolin.allison1875.base.Allison1875;
 import com.spldeolin.allison1875.base.ancestor.Allison1875MainProcessor;
 import com.spldeolin.allison1875.inspector.processor.Inspector;
+import com.spldeolin.allison1875.inspector.statute.Statute;
 
 /**
  * @author Deolin 2020-12-07
@@ -13,6 +17,9 @@ public class InspectorModule extends Allison1875.Module {
     protected final InspectorConfig inspectorConfig;
 
     public InspectorModule(InspectorConfig inspectorConfig) {
+        // multi bind
+        bind(new TypeLiteral<Collection<Statute>>() {
+        }).toInstance(Lists.newArrayList());
         this.inspectorConfig = super.ensureValid(inspectorConfig);
     }
 
