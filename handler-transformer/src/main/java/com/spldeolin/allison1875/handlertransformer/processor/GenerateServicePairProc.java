89827e18601f1330a25528d4d68ce885bd2c75c8
Deolin <splendid.deolin@gmail.com>
2021-03-05 16:22:14 +0800

    GenerateServicePairProc#generateService开发中，"使用指定命名的Service"开发完毕

--- handler-transformer/src/main/java/com/spldeolin/allison1875/handlertransformer/processor/GenerateServicePairProc.java
+++ handler-transformer/src/main/java/com/spldeolin/allison1875/handlertransformer/processor/GenerateServicePairProc.java
@@ -1,12 +1,22 @@
 package com.spldeolin.allison1875.handlertransformer.processor;
 
+import java.nio.file.Path;
 import org.apache.commons.lang3.StringUtils;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.utils.CodeGenerationUtils;
+import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.spldeolin.allison1875.base.constant.AnnotationConstant;
+import com.spldeolin.allison1875.base.util.MoreStringUtils;
 import com.spldeolin.allison1875.base.util.ast.Javadocs;
+import com.spldeolin.allison1875.base.util.ast.Locations;
+import com.spldeolin.allison1875.base.util.ast.Saves;
 import com.spldeolin.allison1875.handlertransformer.HandlerTransformerConfig;
+import com.spldeolin.allison1875.handlertransformer.handle.CreateServiceMethodHandle;
+import com.spldeolin.allison1875.handlertransformer.handle.javabean.CreateServiceMethodHandleResult;
 import com.spldeolin.allison1875.handlertransformer.javabean.FirstLineDto;
 import com.spldeolin.allison1875.handlertransformer.javabean.GenerateServiceImplParam;
 import com.spldeolin.allison1875.handlertransformer.javabean.GenerateServiceParam;
@@ -27,6 +37,12 @@ public class GenerateServicePairProc {
 
     @Inject
     private HandlerTransformerConfig conf;
+
+    @Inject
+    private CreateServiceMethodHandle createServiceMethodHandle;
+
+    @Inject
+    private EnsureNoRepeatProc ensureNoRepeatProc;
 
     public ServiceGeneration generateService(GenerateServiceParam param) {
         FirstLineDto firstLineDto = param.getFirstLineDto();
@@ -53,18 +69,55 @@ public class GenerateServicePairProc {
                 serviceCu.setImports(param.getCu().getImports());
                 ClassOrInterfaceDeclaration service = new ClassOrInterfaceDeclaration();
                 service.setJavadocComment(Javadocs.createJavadoc("", conf.getAuthor()));
-                service.setPublic(true).setStatic(false).setInterface(true).setName(serviceName);
+                service.setPublic(true).setStatic(false).setInterface(true)
+                        .setName(standardizeServiceName(serviceName));
+                serviceCu.setType(0, service);
+                Path storage = Locations.getStorage(param.getCu()).getPath();
+                storage = storage.resolve(CodeGenerationUtils.packageToPath(conf.getServicePackage()));
+                storage = storage.resolve(service.getName() + ".java");
+                serviceCu.setStorage(storage);
+                Saves.add(serviceCu);
+                log.info("generate Service [{}]", service.getName());
 
-
-                // TODO 创建Service Pair
+                CompilationUnit serviceImplCu = new CompilationUnit();
+                serviceImplCu.setPackageDeclaration(conf.getServiceImplPackage());
+                serviceImplCu.setImports(param.getCu().getImports());
+                serviceImplCu.addImport(AnnotationConstant.SLF4J_QUALIFIER);
+                ClassOrInterfaceDeclaration serviceImpl = new ClassOrInterfaceDeclaration();
+                serviceImpl.setJavadocComment(Javadocs.createJavadoc("", conf.getAuthor()));
+                serviceImpl.addAnnotation(AnnotationConstant.OVERRIDE);
+                serviceImpl.addAnnotation(AnnotationConstant.SLF4J);
+                serviceImpl.setPublic(true).setStatic(false).setInterface(false).setName(service.getName() + "Impl");
+                serviceImplCu.setType(0, serviceImpl);
+                storage = Locations.getStorage(param.getCu()).getPath();
+                storage = storage.resolve(CodeGenerationUtils.packageToPath(conf.getServiceImplPackage()));
+                storage = storage.resolve(serviceImpl.getName() + ".java");
+                serviceImplCu.setStorage(storage);
+                Saves.add(serviceCu);
+                log.info("generate ServiceImpl [{}]", serviceImpl.getName());
+                pair = new ServicePairDto().setService(service).setServiceImpls(Lists.newArrayList(serviceImpl));
             }
 
         } else {
             // 创建Single Method Service
-            // TODO 创建Service Pair
+
+            pair = new ServicePairDto();
         }
 
         // TODO 调用handle创建Service Method，添加到ServicePair中
+        CreateServiceMethodHandleResult methodGeneration = createServiceMethodHandle
+                .createMethodImpl(firstLineDto, param.getReqDtoRespDtoInfo().getParamType(),
+                        param.getReqDtoRespDtoInfo().getResultType());
+        // 方法名去重
+        String noRepeat = ensureNoRepeatProc
+                .inService(pair.getService(), methodGeneration.getServiceMethod().getNameAsString());
+        methodGeneration.getServiceMethod().setName(noRepeat);
+
+        MethodDeclaration serviceMethodImpl = methodGeneration.getServiceMethod();
+        MethodDeclaration method = new MethodDeclaration().setType(serviceMethodImpl.getType())
+                .setName(serviceMethodImpl.getName()).setParameters(serviceMethodImpl.getParameters());
+        method.setBody(null);
+        pair.getService().addMember(method);
 
         ServiceGeneration result = new ServiceGeneration();
         return result;
@@ -74,6 +127,35 @@ public class GenerateServicePairProc {
 
         ServiceImplGeneration result = new ServiceImplGeneration();
         return result;
+    }
+
+    private String standardizeServiceName(String serviceName) {
+        String result = MoreStringUtils.upperFirstLetter(serviceName);
+        if (!result.endsWith("Service")) {
+            result += "Service";
+        }
+        // report standardize
+        if (!result.equals(serviceName)) {
+            log.info("Service name standardize from [{}] to [{}]", serviceName, result);
+        }
+        return result;
+    }
+
+    private String calcServiceName(FirstLineDto firstLineDto) {
+        String serviceName;
+        if (StringUtils.isNotBlank(firstLineDto.getServiceName())) {
+            serviceName = MoreStringUtils.upperFirstLetter(firstLineDto.getServiceName());
+            if (!serviceName.endsWith("Service")) {
+                serviceName += "Service";
+            }
+            // report standardize
+            if (!serviceName.equals(firstLineDto.getServiceName())) {
+                log.info("Service name standardize from [{}] to [{}]", firstLineDto.getServiceName(), serviceName);
+            }
+        } else {
+            serviceName = MoreStringUtils.upperFirstLetter(firstLineDto.getHandlerName()) + "Service";
+        }
+        return serviceName;
     }
 
 }
