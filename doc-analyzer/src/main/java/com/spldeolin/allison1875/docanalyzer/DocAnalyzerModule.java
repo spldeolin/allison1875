5209621e6ff8fd187266cc85b23971c0a3d8b63b
Deolin <splendid.deolin@gmail.com>
2021-04-09 17:07:20 +0800

    doc-analyzer：删除DocAnalyzerModule#configure中多余的绑定

--- doc-analyzer/src/main/java/com/spldeolin/allison1875/docanalyzer/DocAnalyzerModule.java
+++ doc-analyzer/src/main/java/com/spldeolin/allison1875/docanalyzer/DocAnalyzerModule.java
@@ -4,13 +4,6 @@ import java.util.Set;
 import com.google.common.collect.Sets;
 import com.spldeolin.allison1875.base.ancestor.Allison1875MainProcessor;
 import com.spldeolin.allison1875.base.ancestor.Allison1875Module;
-import com.spldeolin.allison1875.docanalyzer.handle.AccessDescriptionHandle;
-import com.spldeolin.allison1875.docanalyzer.handle.AnalyzeCustomValidationHandle;
-import com.spldeolin.allison1875.docanalyzer.handle.AnalyzeEnumConstantHandle;
-import com.spldeolin.allison1875.docanalyzer.handle.MoreHandlerAnalysisHandle;
-import com.spldeolin.allison1875.docanalyzer.handle.MoreJpdvAnalysisHandle;
-import com.spldeolin.allison1875.docanalyzer.handle.ObtainConcernedResponseBodyHandle;
-import com.spldeolin.allison1875.docanalyzer.handle.SpecificFieldDescriptionsHandle;
 import com.spldeolin.allison1875.docanalyzer.processor.DocAnalyzer;
 import lombok.extern.log4j.Log4j2;
 
@@ -22,13 +15,6 @@ public class DocAnalyzerModule extends Allison1875Module {
 
     @Override
     protected void configure() {
-        bind(AnalyzeCustomValidationHandle.class).to(AnalyzeCustomValidationHandle.class);
-        bind(AnalyzeEnumConstantHandle.class).to(AnalyzeEnumConstantHandle.class);
-        bind(MoreJpdvAnalysisHandle.class).to(MoreJpdvAnalysisHandle.class);
-        bind(ObtainConcernedResponseBodyHandle.class).to(ObtainConcernedResponseBodyHandle.class);
-        bind(SpecificFieldDescriptionsHandle.class).to(SpecificFieldDescriptionsHandle.class);
-        bind(AccessDescriptionHandle.class).to(AccessDescriptionHandle.class);
-        bind(MoreHandlerAnalysisHandle.class).to(MoreHandlerAnalysisHandle.class);
     }
 
     @Override
