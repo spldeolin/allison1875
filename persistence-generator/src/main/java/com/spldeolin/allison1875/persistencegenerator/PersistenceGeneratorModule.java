8f81cb75d9e54730e08a430063df1903012a6617
Deolin <splendid.deolin@gmail.com>
2021-04-14 13:56:33 +0800

    persistence-generator：所有Handle从 interface+singleton 改成 singleton 模式

--- persistence-generator/src/main/java/com/spldeolin/allison1875/persistencegenerator/PersistenceGeneratorModule.java
+++ persistence-generator/src/main/java/com/spldeolin/allison1875/persistencegenerator/PersistenceGeneratorModule.java
@@ -4,12 +4,6 @@ import java.util.Set;
 import com.google.common.collect.Sets;
 import com.spldeolin.allison1875.base.ancestor.Allison1875MainProcessor;
 import com.spldeolin.allison1875.base.ancestor.Allison1875Module;
-import com.spldeolin.allison1875.persistencegenerator.handle.DefaultGenerateEntityFieldHandle;
-import com.spldeolin.allison1875.persistencegenerator.handle.DefaultGenerateQueryDesignFieldHandle;
-import com.spldeolin.allison1875.persistencegenerator.handle.DefaultJdbcTypeHandle;
-import com.spldeolin.allison1875.persistencegenerator.handle.GenerateEntityFieldHandle;
-import com.spldeolin.allison1875.persistencegenerator.handle.GenerateQueryDesignFieldHandle;
-import com.spldeolin.allison1875.persistencegenerator.handle.JdbcTypeHandle;
 import com.spldeolin.allison1875.persistencegenerator.processor.PersistenceGenerator;
 
 /**
@@ -19,9 +13,6 @@ public class PersistenceGeneratorModule extends Allison1875Module {
 
     @Override
     protected void configure() {
-        bind(GenerateEntityFieldHandle.class).to(DefaultGenerateEntityFieldHandle.class);
-        bind(GenerateQueryDesignFieldHandle.class).to(DefaultGenerateQueryDesignFieldHandle.class);
-        bind(JdbcTypeHandle.class).to(DefaultJdbcTypeHandle.class);
     }
 
     @Override
