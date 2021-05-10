96d9d9362892a766d36e7453a0cbe74d6d16cb0d
Deolin <splendid.deolin@gmail.com>
2021-05-10 08:10:49 +0800

    persistence-generator：现在，mapper.xml中的锚点值由表名hash而来，即表名不发生变化，锚点值就不发生变化

--- persistence-generator/src/main/java/com/spldeolin/allison1875/persistencegenerator/processor/AnchorProc.java
+++ persistence-generator/src/main/java/com/spldeolin/allison1875/persistencegenerator/processor/AnchorProc.java
@@ -2,6 +2,7 @@ package com.spldeolin.allison1875.persistencegenerator.processor;
 
 import java.nio.charset.StandardCharsets;
 import org.apache.commons.lang3.StringUtils;
+import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
 import com.google.inject.Singleton;
 import com.spldeolin.allison1875.persistencegenerator.javabean.PersistenceDto;
@@ -12,17 +13,17 @@ import com.spldeolin.allison1875.persistencegenerator.javabean.PersistenceDto;
 @Singleton
 public class AnchorProc {
 
+    private static final HashFunction hash = Hashing.hmacMd5("Allison 1875".getBytes(StandardCharsets.UTF_8));
+
     public String createLeftAnchor(PersistenceDto persistence) {
-        String target = persistence.toString();
-        String hashCode = Hashing.hmacMd5("Allison 1875".getBytes(StandardCharsets.UTF_8))
-                .hashString(target, StandardCharsets.UTF_8).toString();
+        String target = persistence.getMapperName();
+        String hashCode = hash.hashString(target, StandardCharsets.UTF_8).toString();
         return StringUtils.left(hashCode, 6);
     }
 
     public String createRightAnchor(PersistenceDto persistence) {
-        String target = persistence.toString();
-        String hashCode = Hashing.hmacMd5("Allison 1875".getBytes(StandardCharsets.UTF_8))
-                .hashString(target, StandardCharsets.UTF_8).toString();
+        String target = persistence.getMapperName();
+        String hashCode = hash.hashString(target, StandardCharsets.UTF_8).toString();
         return StringUtils.right(hashCode, 6);
     }
 
