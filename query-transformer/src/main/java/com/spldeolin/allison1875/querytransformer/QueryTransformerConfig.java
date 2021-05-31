c51b4e16246964228403e1fc4faa4deec72a0f0d
Deolin <splendid.deolin@gmail.com>
2021-05-31 14:24:28 +0800

    query-transformer：优化配置项

--- query-transformer/src/main/java/com/spldeolin/allison1875/querytransformer/QueryTransformerConfig.java
+++ query-transformer/src/main/java/com/spldeolin/allison1875/querytransformer/QueryTransformerConfig.java
@@ -1,7 +1,6 @@
 package com.spldeolin.allison1875.querytransformer;
 
 import java.util.Map;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
@@ -13,12 +12,6 @@ import lombok.Data;
 @Singleton
 @Data
 public final class QueryTransformerConfig extends AbstractModule {
-
-    /**
-     * mapper.xml所在目录的相对路径（根据目标工程的情况填写）
-     */
-    @NotEmpty
-    private String mapperXmlDirectoryPath;
 
     /**
      * Entity通用属性的类型
