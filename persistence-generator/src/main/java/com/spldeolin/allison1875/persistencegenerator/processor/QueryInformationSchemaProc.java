a1cf802a88b10a0dc918fea90c63562312172ddd
Deolin <splendid.deolin@gmail.com>
2021-06-11 07:54:24 +0800

    persistence-generator：使用jodd-db代替commons-dbutils

--- persistence-generator/src/main/java/com/spldeolin/allison1875/persistencegenerator/processor/QueryInformationSchemaProc.java
+++ persistence-generator/src/main/java/com/spldeolin/allison1875/persistencegenerator/processor/QueryInformationSchemaProc.java
@@ -2,13 +2,7 @@ package com.spldeolin.allison1875.persistencegenerator.processor;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Collection;
-import java.util.List;
 import java.util.stream.Collectors;
-import org.apache.commons.dbutils.BasicRowProcessor;
-import org.apache.commons.dbutils.GenerousBeanProcessor;
-import org.apache.commons.dbutils.QueryRunner;
-import org.apache.commons.dbutils.ResultSetHandler;
-import org.apache.commons.dbutils.handlers.BeanListHandler;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 import com.google.common.io.Resources;
@@ -17,7 +11,12 @@ import com.google.inject.Singleton;
 import com.spldeolin.allison1875.base.util.CollectionUtils;
 import com.spldeolin.allison1875.persistencegenerator.PersistenceGeneratorConfig;
 import com.spldeolin.allison1875.persistencegenerator.javabean.InformationSchemaDto;
-import com.zaxxer.hikari.HikariDataSource;
+import jodd.db.DbOom;
+import jodd.db.DbSession;
+import jodd.db.ThreadDbSessionHolder;
+import jodd.db.ThreadDbSessionProvider;
+import jodd.db.oom.DbOomQuery;
+import jodd.db.pool.CoreConnectionPool;
 import lombok.extern.log4j.Log4j2;
 
 /**
@@ -31,15 +30,15 @@ public class QueryInformationSchemaProc {
     private PersistenceGeneratorConfig persistenceGeneratorConfig;
 
     public Collection<InformationSchemaDto> process() {
-        HikariDataSource dataSource = new HikariDataSource();
-        dataSource.setJdbcUrl(persistenceGeneratorConfig.getJdbcUrl());
-        dataSource.setUsername(persistenceGeneratorConfig.getUserName());
-        dataSource.setPassword(persistenceGeneratorConfig.getPassword());
+        CoreConnectionPool dscp = new CoreConnectionPool();
+        dscp.setDriver("com.mysql.jdbc.Driver");
+        dscp.setUrl(persistenceGeneratorConfig.getJdbcUrl());
+        dscp.setUser(persistenceGeneratorConfig.getUserName());
+        dscp.setPassword(persistenceGeneratorConfig.getPassword());
 
-        ResultSetHandler<List<InformationSchemaDto>> rsh = new BeanListHandler<>(InformationSchemaDto.class,
-                new BasicRowProcessor(new GenerousBeanProcessor()));
-
-        QueryRunner runner = new QueryRunner(dataSource);
+        ThreadDbSessionHolder.set(new DbSession(dscp));
+        DbOom dbOom = DbOom.create().withConnectionProvider(dscp).withSessionProvider(new ThreadDbSessionProvider())
+                .get().connect();
 
         Collection<InformationSchemaDto> infoSchemas;
         try {
@@ -51,14 +50,27 @@ public class QueryInformationSchemaProc {
                 tables = tables.stream().map(one -> "'" + one + "'").collect(Collectors.toList());
                 part = Joiner.on(',').appendTo(new StringBuilder("IN ("), tables).append(")").toString();
             }
-            sql = sql.replaceFirst("\\{}", part);
+            sql = sql.replace("${tableNames}", part);
+            sql = sql.replace("${tableSchema}", "'" + persistenceGeneratorConfig.getSchema() + "'");
 
-            infoSchemas = runner.query(sql, rsh, persistenceGeneratorConfig.getSchema());
+            DbOomQuery query = dbOom.query(sql);
+            infoSchemas = query.list(resultSet -> {
+                InformationSchemaDto result = new InformationSchemaDto();
+                result.setTableName(resultSet.getString("TABLE_NAME"));
+                result.setTableComment(resultSet.getString("TABLE_COMMENT"));
+                result.setColumnName(resultSet.getString("COLUMN_NAME"));
+                result.setDataType(resultSet.getString("DATA_TYPE"));
+                result.setColumnType(resultSet.getString("COLUMN_TYPE"));
+                result.setColumnComment(resultSet.getString("COLUMN_COMMENT"));
+                result.setColumnKey(resultSet.getString("COLUMN_KEY"));
+                result.setCharacterMaximumLength(resultSet.getLong("CHARACTER_MAXIMUM_LENGTH"));
+                result.setIsNullable(resultSet.getString("IS_NULLABLE"));
+                result.setColumnDefault(resultSet.getString("COLUMN_DEFAULT"));
+                return result;
+            });
         } catch (Throwable e) {
-            log.error("ColumnMetaProc.process", e);
+            log.error("QueryInformationSchemaProc.process", e);
             infoSchemas = Lists.newArrayList();
-        } finally {
-            dataSource.close();
         }
         return infoSchemas;
     }
