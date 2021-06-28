29e70971b343ab8cda2113bdf35d622b68aef78f
Deolin <splendid.deolin@gmail.com>
2021-06-28 13:58:14 +0800

    base：删除不再使用的方法

--- base/src/main/java/com/spldeolin/allison1875/base/builder/SingleMethodServiceCuBuilder.java
+++ base/src/main/java/com/spldeolin/allison1875/base/builder/SingleMethodServiceCuBuilder.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import java.util.Objects;
 import java.util.Set;
 import org.apache.commons.lang3.StringUtils;
-import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.ImportDeclaration;
 import com.github.javaparser.ast.NodeList;
@@ -20,7 +19,6 @@ import com.github.javaparser.utils.CodeGenerationUtils;
 import com.github.javaparser.utils.SourceRoot;
 import com.google.common.base.MoreObjects;
 import com.google.common.collect.Sets;
-import com.spldeolin.allison1875.base.constant.AnnotationConstant;
 import com.spldeolin.allison1875.base.exception.QualifierAbsentException;
 import com.spldeolin.allison1875.base.util.MoreStringUtils;
 import jodd.util.StringUtil;
@@ -56,12 +54,6 @@ public class SingleMethodServiceCuBuilder {
 
     private String methodName;
 
-    public SingleMethodServiceCuBuilder sourceRoot(SourceRoot sourceRoot) {
-        Objects.requireNonNull(sourceRoot, "sourceRoot cannot be null.");
-        this.sourceRoot = sourceRoot;
-        return this;
-    }
-
     public SingleMethodServiceCuBuilder sourceRoot(Path sourceRootPath) {
         Objects.requireNonNull(sourceRootPath, "sourceRootPath cannot be null.");
         this.sourceRoot = new SourceRoot(sourceRootPath);
@@ -74,19 +66,9 @@ public class SingleMethodServiceCuBuilder {
         return this;
     }
 
-    public SingleMethodServiceCuBuilder servicePackageDeclaration(PackageDeclaration packageDeclaration) {
-        this.servicePackageDeclaration = packageDeclaration;
-        return this;
-    }
-
     public SingleMethodServiceCuBuilder implPackageDeclaration(String packageName) {
         Objects.requireNonNull(packageName, "packageName cannot be null.");
         this.implPackageDeclaration = new PackageDeclaration().setName(packageName);
-        return this;
-    }
-
-    public SingleMethodServiceCuBuilder implPackageDeclaration(PackageDeclaration packageDeclaration) {
-        this.implPackageDeclaration = packageDeclaration;
         return this;
     }
 
@@ -123,21 +105,6 @@ public class SingleMethodServiceCuBuilder {
         Javadoc javadoc = new JavadocComment(javadocDescription).parse()
                 .addBlockTag(new JavadocBlockTag(JavadocBlockTag.Type.AUTHOR, author));
         this.javadoc = javadoc;
-        return this;
-    }
-
-    public SingleMethodServiceCuBuilder javadoc(Javadoc javadoc) {
-        this.javadoc = javadoc;
-        return this;
-    }
-
-    public SingleMethodServiceCuBuilder annotationExpr(AnnotationExpr annotationExpr) {
-        annotationExprs.add(annotationExpr);
-        return this;
-    }
-
-    public SingleMethodServiceCuBuilder annotationExpr(String annotation, String... var) {
-        annotationExprs.add(StaticJavaParser.parseAnnotation(String.format(annotation, (Object) var)));
         return this;
     }
 
@@ -199,83 +166,11 @@ public class SingleMethodServiceCuBuilder {
         return result;
     }
 
-    public CompilationUnit buildServiceImpl() {
-        CompilationUnit result = new CompilationUnit();
-
-        // package声明
-        result.setPackageDeclaration(implPackageDeclaration);
-
-        // import声明
-        result.setImports(new NodeList<>(importDeclarations));
-        result.addImport(AnnotationConstant.SLF4J_QUALIFIER);
-        result.addImport(AnnotationConstant.SERVICE_QUALIFIER);
-        result.addImport(serviceQualifier);
-
-        serviceImpl = new ClassOrInterfaceDeclaration();
-
-        // 类级Javadoc
-        if (javadoc != null) {
-            serviceImpl.setJavadocComment(javadoc);
-        }
-
-        // 类级注解
-        annotationExprs.forEach(serviceImpl::addAnnotation);
-        serviceImpl.addAnnotation(AnnotationConstant.SLF4J);
-        serviceImpl.addAnnotation(AnnotationConstant.SERVICE);
-
-        // 类签名
-        serviceImpl.setPublic(true).setStatic(false).setInterface(false).setName(serviceName + "Impl")
-                .addImplementedType(serviceName);
-
-        // 方法签名
-        serviceImpl.addMember(this.method);
-
-        result.setTypes(new NodeList<>(serviceImpl));
-
-        // CU的路径
-        Path storage = sourceRoot.getRoot();
-        if (implPackageDeclaration != null) {
-            storage = storage.resolve(CodeGenerationUtils.packageToPath(implPackageDeclaration.getNameAsString()));
-        }
-        storage = storage.resolve(serviceImpl.getNameAsString() + ".java");
-        result.setStorage(storage);
-
-        return result;
-    }
-
     public ClassOrInterfaceDeclaration getService() {
         if (service == null) {
             throw new IllegalStateException("buildService() not yet.");
         }
         return service;
-    }
-
-    public ClassOrInterfaceDeclaration getServiceImpl() {
-        if (serviceImpl == null) {
-            throw new IllegalArgumentException("buildServiceImpl() net yet.");
-        }
-        return serviceImpl;
-    }
-
-    public String getJavabeanQualifier() {
-        if (service == null) {
-            throw new IllegalStateException("buildService() not yet.");
-        }
-        return serviceQualifier;
-    }
-
-    public String getServiceVarName() {
-        if (service == null) {
-            throw new IllegalArgumentException("buildService() not yet.");
-        }
-        return serviceVarName;
-    }
-
-    public String getMethodName() {
-        if (service == null) {
-            throw new IllegalArgumentException("buildService() not yet.");
-        }
-        return methodName;
     }
 
 }
