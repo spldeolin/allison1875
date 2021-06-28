d05de6c1228eb8e67828c1a611643a5ef38dac73
Deolin <splendid.deolin@gmail.com>
2021-06-28 14:51:50 +0800

    persistence-generator：修复了BatchUpdateXmlProc中对半角逗号的错误处理

--- persistence-generator/src/main/java/com/spldeolin/allison1875/persistencegenerator/processor/mapperxml/BatchUpdateXmlProc.java
+++ persistence-generator/src/main/java/com/spldeolin/allison1875/persistencegenerator/processor/mapperxml/BatchUpdateXmlProc.java
@@ -9,7 +9,6 @@ import com.spldeolin.allison1875.base.constant.BaseConstant;
 import com.spldeolin.allison1875.persistencegenerator.PersistenceGeneratorConfig;
 import com.spldeolin.allison1875.persistencegenerator.facade.javabean.PropertyDto;
 import com.spldeolin.allison1875.persistencegenerator.javabean.PersistenceDto;
-import jodd.util.StringUtil;
 
 /**
  * 插入
@@ -31,17 +30,13 @@ public class BatchUpdateXmlProc {
         xmlLines.add(BaseConstant.SINGLE_INDENT + "<foreach collection=\"entities\" item=\"one\" separator=\";\">");
         xmlLines.add(BaseConstant.DOUBLE_INDENT + BaseConstant.FORMATTER_OFF_MARKER);
         xmlLines.add(BaseConstant.DOUBLE_INDENT + "UPDATE " + persistence.getTableName());
-        xmlLines.add(BaseConstant.DOUBLE_INDENT + "SET");
+        xmlLines.add(BaseConstant.DOUBLE_INDENT + "<set>");
         for (PropertyDto nonId : persistence.getNonIdProperties()) {
             xmlLines.add(BaseConstant.TREBLE_INDENT + String
                     .format("<if test=\"one.%s!=null\"> %s = #{one.%s}, </if>", nonId.getPropertyName(),
                             nonId.getColumnName(), nonId.getPropertyName()));
         }
-        // 删除最后一个语句中，最后的逗号
-        if (xmlLines.size() > 0) {
-            int last = xmlLines.size() - 1;
-            xmlLines.set(last, StringUtil.cutSuffix(xmlLines.get(last), ", </if>") + " </if>");
-        }
+        xmlLines.add(BaseConstant.DOUBLE_INDENT + "</set>");
         xmlLines.add(BaseConstant.DOUBLE_INDENT + "WHERE TRUE");
         if (persistence.getIsDeleteFlagExist()) {
             xmlLines.add(BaseConstant.DOUBLE_INDENT + "  AND " + persistenceGeneratorConfig.getNotDeletedSql());
