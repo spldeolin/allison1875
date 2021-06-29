3a6d55a4ae97cfb0ab06e238998b0fc8e7ebf327
Deolin <splendid.deolin@gmail.com>
2021-06-29 13:25:33 +0800

    doc-analyzer：追加异常时的warn

--- doc-analyzer/src/main/java/com/spldeolin/allison1875/docanalyzer/processor/MethodCollectProc.java
+++ doc-analyzer/src/main/java/com/spldeolin/allison1875/docanalyzer/processor/MethodCollectProc.java
@@ -6,6 +6,7 @@ import com.github.javaparser.ast.body.MethodDeclaration;
 import com.google.common.collect.Maps;
 import com.google.inject.Singleton;
 import com.spldeolin.allison1875.base.util.ast.MethodQualifiers;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 内聚了 收集coid下所有method的功能
@@ -13,12 +14,17 @@ import com.spldeolin.allison1875.base.util.ast.MethodQualifiers;
  * @author Deolin 2020-06-10
  */
 @Singleton
+@Slf4j
 public class MethodCollectProc {
 
     public Map<String, MethodDeclaration> collectMethods(ClassOrInterfaceDeclaration coid) {
         Map<String, MethodDeclaration> methods = Maps.newHashMap();
         for (MethodDeclaration method : coid.findAll(MethodDeclaration.class)) {
-            methods.put(MethodQualifiers.getShortestQualifiedSignature(method), method);
+            try {
+                methods.put(MethodQualifiers.getShortestQualifiedSignature(method), method);
+            } catch (Exception e) {
+                log.warn("fail to get shortest qualified signature [{}]", method.getNameAsString(), e);
+            }
         }
         return methods;
     }
