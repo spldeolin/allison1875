9ff8d55752387b3496a305dc23a9637db6c74059
Deolin <splendid.deolin@gmail.com>
2023-11-03 14:08:37 +0800

    MAKE: release 9.0

--- base/src/main/java/com/spldeolin/allison1875/base/Version.java
+++ base/src/main/java/com/spldeolin/allison1875/base/Version.java
@@ -8,9 +8,9 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class Version {
 
-    public static final String logDisplayVersion = "Allison 1875 9.0-SNAPSHOT";
+    public static final String logDisplayVersion = "Allison 1875 9.0";
 
-    public static final String lotNoVersion = "0900S";
+    public static final String lotNoVersion = "0900R";
 
     public static void greeting() {
         log.info(logDisplayVersion);
