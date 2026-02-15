e56a056f3d5226c00b644292a5501f9b5d445273
Deolin <splendid.deolin@gmail.com>
2026-02-15 18:54:03 +0800

    feat: 增强表单生成器的请求响应结构生成功能
    1. 为列表查询接口添加请求响应结构生成，包含分页参数和列表项字段
    2. 为详情查询接口添加请求响应结构生成，支持通过业务代码获取详情
    3. 为删除接口添加请求结构生成，支持批量删除功能
    4. 引入 AnnotationExprService 服务用于生成校验注解（@NotNull、@NotEmpty）
    5. 过滤 SECRET 类型字段，确保敏感信息不出现在接口请求响应中

--- form-generator/src/main/java/com/spldeolin/allison1875/formgenerator/service/impl/InitDecServiceImpl.java
+++ form-generator/src/main/java/com/spldeolin/allison1875/formgenerator/service/impl/InitDecServiceImpl.java
@@ -10,12 +10,14 @@ import com.github.javaparser.ast.stmt.LocalClassDeclarationStmt;
 import com.github.javaparser.utils.StringEscapeUtils;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.spldeolin.allison1875.common.service.AnnotationExprService;
 import com.spldeolin.allison1875.common.util.JavadocUtils;
 import com.spldeolin.allison1875.common.util.JsonUtils;
 import com.spldeolin.allison1875.common.util.MoreStringUtils;
 import com.spldeolin.allison1875.formgenerator.dsl.FormDef;
 import com.spldeolin.allison1875.formgenerator.dsl.ItemDef;
 import com.spldeolin.allison1875.formgenerator.dsl.enums.InitOrEditPattern;
+import com.spldeolin.allison1875.formgenerator.dsl.enums.ItemType;
 import com.spldeolin.allison1875.formgenerator.service.InitDecService;
 import com.spldeolin.allison1875.formgenerator.service.ItemService;
 
@@ -27,6 +29,9 @@ public class InitDecServiceImpl implements InitDecService {
 
     @Inject
     private ItemService<ItemDef> itemService;
+
+    @Inject
+    private AnnotationExprService annotationExprService;
 
     @Override
     public InitializerDeclaration buildSaveHandler(FormDef form) {
@@ -68,6 +73,40 @@ public class InitDecServiceImpl implements InitDecService {
                 String.format("String handler = \"list%s\", desc = \"%s列表\", form=\"%s\";",
                         MoreStringUtils.toUpperCamel(form.getName()), form.getTitle(),
                         StringEscapeUtils.escapeJava(JsonUtils.toJson(form)))));
+
+        // req声明
+        ClassOrInterfaceDeclaration reqCoid = new ClassOrInterfaceDeclaration().setName("req");
+        for (ItemDef item : form.getItems()) {
+            if (item.getType() == ItemType.SECRET) {
+                continue;
+            }
+            FieldDeclaration itemField = StaticJavaParser.parseBodyDeclaration(
+                    itemService.getJavaTypeInDTO(item) + " " + item.getName() + ";").asFieldDeclaration();
+            JavadocUtils.setJavadoc(itemField, item.getTitle(), null);
+            itemService.getJavaJsonFormatAnnoatation(item).ifPresent(itemField::addAnnotation);
+            reqCoid.addMember(itemField);
+        }
+        FieldDeclaration pageNum = StaticJavaParser.parseBodyDeclaration("Integer pageNum;").asFieldDeclaration();
+        JavadocUtils.setJavadoc(pageNum, "分页页码", null);
+        reqCoid.addMember(pageNum);
+        FieldDeclaration pageSize = StaticJavaParser.parseBodyDeclaration("Integer pageSize;").asFieldDeclaration();
+        JavadocUtils.setJavadoc(pageSize, "分页条数", null);
+        reqCoid.addMember(pageSize);
+        bs.addStatement(new LocalClassDeclarationStmt(reqCoid));
+
+        // resp声明
+        ClassOrInterfaceDeclaration respCoid = new ClassOrInterfaceDeclaration().setName("resp");
+        for (ItemDef item : form.getItems()) {
+            if (item.getType() == ItemType.SECRET) {
+                continue;
+            }
+            FieldDeclaration itemField = StaticJavaParser.parseBodyDeclaration(
+                    itemService.getJavaTypeInDTO(item) + " " + item.getName() + ";").asFieldDeclaration();
+            JavadocUtils.setJavadoc(itemField, item.getTitle(), null);
+            itemService.getJavaJsonFormatAnnoatation(item).ifPresent(itemField::addAnnotation);
+            respCoid.addMember(itemField);
+        }
+        bs.addStatement(new LocalClassDeclarationStmt(respCoid.addAnnotation("com.spldeolin.allison1875.support.P")));
         return new InitializerDeclaration(false, bs);
     }
 
@@ -78,6 +117,27 @@ public class InitDecServiceImpl implements InitDecService {
                 String.format("String handler = \"get%sDetail\", desc = \"%s详情\", form=\"%s\";",
                         MoreStringUtils.toUpperCamel(form.getName()), form.getTitle(),
                         StringEscapeUtils.escapeJava(JsonUtils.toJson(form)))));
+
+        // req声明
+        FieldDeclaration bizIdField = StaticJavaParser.parseBodyDeclaration(
+                "String " + StringUtils.uncapitalize(form.getName()) + "Code;").asFieldDeclaration();
+        ClassOrInterfaceDeclaration reqCoid = new ClassOrInterfaceDeclaration().setName("req")
+                .addMember(bizIdField.clone().addAnnotation(annotationExprService.notNull()));
+        bs.addStatement(new LocalClassDeclarationStmt(reqCoid));
+
+        // resp声明
+        ClassOrInterfaceDeclaration respCoid = new ClassOrInterfaceDeclaration().setName("resp");
+        for (ItemDef item : form.getItems()) {
+            if (item.getType() == ItemType.SECRET) {
+                continue;
+            }
+            FieldDeclaration itemField = StaticJavaParser.parseBodyDeclaration(
+                    itemService.getJavaTypeInDTO(item) + " " + item.getName() + ";").asFieldDeclaration();
+            JavadocUtils.setJavadoc(itemField, item.getTitle(), null);
+            itemService.getJavaJsonFormatAnnoatation(item).ifPresent(itemField::addAnnotation);
+            respCoid.addMember(itemField);
+        }
+        bs.addStatement(new LocalClassDeclarationStmt(respCoid));
         return new InitializerDeclaration(false, bs);
     }
 
@@ -88,6 +148,13 @@ public class InitDecServiceImpl implements InitDecService {
                 String.format("String handler = \"delete%s\", desc = \"删除%s\", form=\"%s\";",
                         MoreStringUtils.toUpperCamel(form.getName()), form.getTitle(),
                         StringEscapeUtils.escapeJava(JsonUtils.toJson(form)))));
+
+        // req声明
+        FieldDeclaration bizIdField = StaticJavaParser.parseBodyDeclaration(
+                "java.util.List<String> " + StringUtils.uncapitalize(form.getName()) + "Codes;").asFieldDeclaration();
+        ClassOrInterfaceDeclaration reqCoid = new ClassOrInterfaceDeclaration().setName("req")
+                .addMember(bizIdField.clone().addAnnotation(annotationExprService.notEmpty()));
+        bs.addStatement(new LocalClassDeclarationStmt(reqCoid));
         return new InitializerDeclaration(false, bs);
     }
 
