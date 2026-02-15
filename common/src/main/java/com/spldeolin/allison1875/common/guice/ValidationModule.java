dbfea0ceafeed03654afeff2548dfa73f026f6db
Deolin <splendid.deolin@gmail.com>
2026-02-15 14:16:53 +0800

    refactor: 优化方法拦截器，排除 synthetic 方法

--- common/src/main/java/com/spldeolin/allison1875/common/guice/ValidationModule.java
+++ common/src/main/java/com/spldeolin/allison1875/common/guice/ValidationModule.java
@@ -1,6 +1,8 @@
 package com.spldeolin.allison1875.common.guice;
 
+import java.lang.reflect.Method;
 import com.google.inject.AbstractModule;
+import com.google.inject.matcher.AbstractMatcher;
 import com.google.inject.matcher.Matchers;
 
 /**
@@ -13,8 +15,13 @@ public class ValidationModule extends AbstractModule {
         // 注册Singleton校验监听器
         bindListener(Matchers.any(), new ValidSingletonListener());
 
-        // 注册方法切面
-        bindInterceptor(Matchers.any(), Matchers.any(), new ValidMethodArgsInterceptor());
+        // 注册方法切面，排除synthetic方法（如泛型擦除产生的bridge方法）
+        bindInterceptor(Matchers.any(), new AbstractMatcher<Method>() {
+            @Override
+            public boolean matches(Method method) {
+                return !method.isSynthetic();
+            }
+        }, new ValidMethodArgsInterceptor());
     }
 
 }
