29ed459622f70782bcfd428b599a07811dfc2045
Deolin <splendid.deolin@gmail.com>
2026-05-21 14:11:49 +0800

    remove facade modules

--- handler-transformer/src/main/java/com/spldeolin/allison1875/handlertransformer/dto/GenerateServiceAndImplArgs.java
+++ handler-transformer/src/main/java/com/spldeolin/allison1875/handlertransformer/dto/GenerateServiceAndImplArgs.java
@@ -14,6 +14,6 @@ public class GenerateServiceAndImplArgs {
 
     CompilationUnit controllerCu;
 
-    com.spldeolin.allison1875.handlertransformer.dto.InitDecAnalysisDTO initDecAnalysisDTO;
+    InitDecAnalysisDTO initDecAnalysisDTO;
 
 }
