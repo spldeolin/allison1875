645a071a21229267e63c722bd8326212cdad7d9f
Deolin <splendid.deolin@gmail.com>
2026-05-30 22:03:26 +0800

    fix(form-generator): use Spring StringUtils.hasText for secret item validation (match text item)

--- form-generator/src/main/java/com/spldeolin/allison1875/formgenerator/service/impl/SecretItemService.java
+++ form-generator/src/main/java/com/spldeolin/allison1875/formgenerator/service/impl/SecretItemService.java
@@ -78,7 +78,7 @@ public class SecretItemService implements ItemService<SecretItemDef> {
     @Override
     public Statement getValidationStatement(SecretItemDef itemDef) {
         return parseStatement(
-                "if (org.apache.commons.lang3.StringUtils.isBlank(req.get%s())) { throw new IllegalArgumentException(\"%s不能为空\"); }",
+                "if (!StringUtils.hasText(req.get%s())) { throw new IllegalArgumentException(\"%s不能为空\"); }",
                 StringUtils.capitalize(itemDef.getName()), itemDef.getTitle());
     }
 
